1d89e8b5b691c6aab0652632b2bd7ef13cc2e3e8
et9719 <ella-t@live.co.uk>
2022-03-24 11:43:04 +0000

    create post comment and newspost models, add them into admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,55 @@
+''' Imports '''
+from django_summernote.admin import SummernoteModelAdmin
 from django.contrib import admin
+from .models import Post, Comment, NewsPost
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    ''' Details of what our admin can see/do with our post model '''
+    # What we can see when we a shown the list of posts on admin site.
+    list_display = ('title', 'slug', 'status', 'created_on')
+    # What we can search for on admin site.
+    search_fields = ['title', 'content']
+    # Generate the slugfield automaticly from the title of a post.
+    prepopulated_fields = {'slug': ('title',)}
+    # Create a filter box that you can filter posts via their status(Published, Draft).
+    # or by the date they were created on.
+    list_filter = ('status', 'created_on')
+    # Summernote is a simple editor on bootstrap, here we allow the user to use this
+    # editor to create content.
+    summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    ''' Details of what our admin can see/do with our post model '''
+    # What we can see when we a shown the list of comments on admin site.
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    # filter posts via approved or by the date they were created on.
+    list_filter = ('approved', 'created_on')
+    # What we can search for on admin site.
+    search_fields = ['name', 'email', 'address', 'body']
+    # Allow admin to approve comments
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        ''' Define what we want the approve comment function to do '''
+        queryset.update(approved=True)
+
+
+@admin.register(NewsPost)
+class NewsPostAdmin(SummernoteModelAdmin):
+    ''' Details of what our admin can see/do with our post model '''
+    # What we can see when we a shown the list of posts on admin site.
+    list_display = ('title', 'slug', 'status', 'created_on')
+    # What we can search for on admin site.
+    search_fields = ['title', 'content']
+    # Generate the slugfield automaticly from the title of a post.
+    prepopulated_fields = {'slug': ('title',)}
+    # Create a filter box that you can filter posts via their status(Published, Draft).
+    # or by the date they were created on.
+    list_filter = ('status', 'created_on')
+    # Summernote is a simple editor on bootstrap, here we allow the user to use this
+    # editor to create content.
+    summernote_fields = ('content')
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))  # Defining status for the post model.
+
+
+class Post(models.Model):
+    ''' All information needed to create a post '''
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    artist = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    updated_on = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+
+    class Meta:
+        ''' Define how you want to order the posts on the site '''
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        ''' Add 1 to likes everytime a post is liked by a user '''
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    '''  All information needed to create a comment'''
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+    )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ''' Define how you want to order the posts on the site '''
+        ordering = ["created_on"]
+
+    def __str__(self):
+        ''' How we want the user to see a comment '''
+        return f"Comment {self.body} by {self.name}"
+
+
+class NewsPost(models.Model):
+    ''' All information needed to create a post '''
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    # artist = models.ForeignKey(
+    #     User, on_delete=models.CASCADE, related_name="blog_posts"
+    # )
+    updated_on = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ''' Define how you want to order the posts on the site '''
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
