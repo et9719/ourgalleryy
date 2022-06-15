5846d179989986e31c0b0e286052adc44888969b
et9719 <ella-t@live.co.uk>
2022-06-15 12:31:18 +0000

    change static url

--- ourgalleryy/settings.py
+++ ourgalleryy/settings.py
@@ -164,7 +164,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/css/'
 
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
