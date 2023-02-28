702ab069e12d70664903c48608fd2a690f574513
et9719 <ella-t@live.co.uk>
2023-02-28 17:36:48 +0000

    push build.sh file

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn ourgalleryy.wsgi
--- ourgalleryy/settings.py
+++ ourgalleryy/settings.py
@@ -33,8 +33,11 @@ DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ["ourgalleryy.herokuapp.com", "localhost"]
-
+ALLOWED_HOSTS = [""]
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
